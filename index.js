51d3f417bfdcde8ec2aec89f8abef48e4288324c
marjanhasan <moonmarjanhasan@gmail.com>
2023-03-14 00:22:45 +0600

    project done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('c')
+let count = 0;
+const num = document.getElementById('number');
+const initial = () => {
+    count = localStorage.getItem('num');
+    num.innerText = count;
+}
+const add = () => {
+    num.innerText = ++count;
+    localStorage.setItem('num',count);
+}
+const clr = () => {
+    count = 0;
+    num.innerText = count;
+    localStorage.setItem('num',count);
+}
+const minus = () => {
+    num.innerText = --count;
+    localStorage.setItem('num',count);
+}
+
